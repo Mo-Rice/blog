61cf5acad6c4c0477b94d4c87499792946bf86c2
Mauricio Diaz-Ortiz Jr <m.diazortiz@student.science.ru.nl>
2020-11-24 12:59:09 +0100

    Added xcode files

---
+++ Documents/Cornucopia/HornOfPlenty/HornOfPlenty/Shared/HornOfPlentyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HornOfPlentyApp.swift
+//  Shared
+//
+//  Created by Mauricio Diaz-Ortiz Jr. on 11/24/20.
+//
+
+import SwiftUI
+
+@main
+struct HornOfPlentyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
